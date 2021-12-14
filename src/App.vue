5f192369858915a9916366ef0d157aab1738e913
korea <643949593@qq.com>
2021-12-14 17:26:04 +0800

    feat(all page): build app basic page
    nothing

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="assets/logo.png" />
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    onMounted(() => {
+      console.log(1);
+    });
+    return {};
+  },
+});
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
