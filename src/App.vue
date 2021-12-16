8f2c0b369a5e3f15b768da055a1c79d90d6a67c5
korea <643949593@qq.com>
2021-12-16 14:48:23 +0800

    feat(korea): basic template edit success
    success

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "App",
-  setup() {
-    onMounted(() => {
-      console.log(1);
-    });
-    return {};
-  },
 });
 </script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app {
+  height: 100%;
+  width: 100%;
 }
 </style>
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>登录</div>
-  <el-button type="primary">哈哈</el-button>
+  <div class="login"></div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -8,4 +7,13 @@ import { defineComponent } from "vue";
 export default defineComponent({});
 </script>
 
-<style lang="less"></style>
+<style lang="less" scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: url("../../assets/img/bg.png");
+}
+</style>
